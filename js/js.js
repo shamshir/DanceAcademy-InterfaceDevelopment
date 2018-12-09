f0b5cb1aeb1985c91d5ebe3b1598acdc4ed1b88c
Javi <javi_vavi@hotmail.com>
2018-12-09 18:09:43 +0100

    Scroll Button Added

--- js/js.js
+++ js/js.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
+    /* Scroll Button management */
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            $("#scrollButton").show();
+        } else {
+            $("#scrollButton").hide();
+        }
+    });
+    $("#scrollButton").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
 });
